40d19b900ce4da53560cd5ed3cff8195d594b1ab
Stijn <Stijn.EXE@gmail.com>
2018-04-21 15:07:11 +0200

    Changed way commands are handled in template

--- discat-modules/Template/module.js
+++ discat-modules/Template/module.js
@@ -1,11 +1,22 @@
 module.exports = {
+    // Every module should contain one function: getCommands, which returns an array of functions (commands)
     getCommands() {
-        return [{
+        var commands = [{
             command: "ping",
-            reply: "pong"
-        }, {
-            command: "ping",
-            reply: "pong"
+            reply: function ping(msg){
+                msg.reply("pong");
+            }
         }];
+
+        if ("@pong" == true){  // Variable linked to serversettings.json
+            commands.push({
+                command: "pong",
+                reply: "ping"
+            });
+        }
+        
+        return commands;
     }
+
+    
 };
