c8b7ba791fc5918255768537777632762039541c
Stijn <Stijn.EXE@gmail.com>
2018-04-22 21:19:55 +0200

    Added missing , that caused spawn() crash

--- discat.js
+++ discat.js
@@ -58,7 +58,7 @@ app.post("/discatupdate", function (req, res) {
         var moveNginxConf = spawn("cp", ["nginx/nginx.conf", "/etc/nginx/nginx.conf"]);  // Pull the new update from github
         moveNginxConf.on("exit", function () {  // Once the update is pulled
           console.log("[] Moving 2...");
-          var moveDiscatConf = spawn("cp" ["nginx/discat.conf", "/etc/nginx/conf.d/discat.conf"]);  // copy nginx.confto the required directory
+          var moveDiscatConf = spawn("cp", ["nginx/discat.conf",  "/etc/nginx/conf.d/discat.conf"]);  // copy nginx.conf to the required directory
           moveDiscatConf.on("exit", function () {  // Once the config files have been moved
             console.log("[] Reloading nginx...");
             var reloadNginx = spawn("sudo" ["nginx", "-s", "reload"]);  // Reload nginx
