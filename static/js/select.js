1aa80537f29039332b1403cd9aefe10c62bce232
Stijn <Stijn.EXE@gmail.com>
2018-05-01 14:56:46 +0200

    Fixed bugs in select.js

--- static/js/select.js
+++ static/js/select.js
@@ -1,10 +1,11 @@
 var request = new XMLHttpRequest();
 request.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("menu").visibility = "hidden";
+        document.getElementById("menu").style.visibility = "hidden";
         console.log(request.responseText);
     }
 }
 function requestOwnedServers() {
     request.open("GET", "/server", true);
+    request.send();
 }
