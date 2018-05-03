f2fd65b378b3dc93cd47c56b479f5105e199ae79
Stijn <Stijn.EXE@gmail.com>
2018-05-03 19:04:17 +0200

    Testing getting document.body.id from pug

--- static/js/server.js
+++ static/js/server.js
@@ -3,10 +3,12 @@ request.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         document.getElementById("menu").innerHTML = request.responseText;
 
+        /*
         // Set server ID in all the modules (may change later in other menus)
         var modulelinks = document.getElementsByClassName("module");
         for (var i = 0; i < modulelinks.length; i++)
             modulelinks[i].href = modulelinks[i].href.replace("{0}", window.location.href.split("=")[1]);
+            */
     }
 }
 
