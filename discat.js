2cf567a9904e59d5384027d20715e518e314d23e
StijnDotExe <stijn.exe@gmail.com>
2018-05-24 13:33:06 +0200

    Don't run checkIfUserLoggedIn unless user has accesstoken

--- discat.js
+++ discat.js
@@ -188,8 +188,9 @@ app.use(session({
 app.get("/login", (req, res) => {
   // If a code is passed to exchange for access token, exchange it before checkIfUserLoggedIn attempts to use it
   if (req.query.code != undefined) exchangeToken(req, res, "authorization_code");
-  else if (checkIfUserLoggedIn(req, res)) {  // If user is logged in
-    res.redirect("/select");  // let him select server or user settings
+  else if (req.session.accessToken != null) {  // If user is logged in
+    if (checkIfUserLoggedIn(req, res))  // Check if valid login
+      res.redirect("/select");  // let him select server or user settings
   }  // If user isn't logged in
   else res.redirect(  // Redirect him to the Discord authentication, which will redirect back to /login
     "https://discordapp.com/api/oauth2/authorize?" +
